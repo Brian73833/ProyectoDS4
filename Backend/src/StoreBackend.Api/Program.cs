d9096099aac86439b2ab8003c7e31194a3b67f3b
BrianPorras <brianporras6019@gmail.com>
2026-05-24 20:58:19 -0600

    Estructura basica del proyecto

---
+++ Backend/src/StoreBackend.Api/Program.cs
@@ -0,0 +1,34 @@
+using System.Text;
+using System.Threading.RateLimiting;
+using Microsoft.AspNetCore.RateLimiting;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using StoreBackend.DomainService;
+using StoreBackend.Facade;
+using StoreBackend.Infrastructure;
+using StoreBackend.Infrastructure.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
